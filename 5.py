994ebbb78a8aa6e4e190f298efd00e30da74f2e4
rizkywidodo <74779025+rizkywidodo@users.noreply.github.com>
2022-03-31 16:21:36 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,10 +1,14 @@
+from bs4 import BeautifulSoup
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("www.classroom.its.ac.id/my/", 80))
-sock.send(b"GET / HTTP/1.1\r\nHost:www.classroom.its.ac.id/my/\r\n\r\n")
+url = "classroom.its.ac.id"
+sock.connect((url, 80))
+sock.send(b"GET / HTTP/1.1\r\nHost:classroom.its.ac.id\r\n\r\n")
 response = sock.recv(4096)
-sock.close()
-print(response.decode())
 
-# BELUM
+soup = BeautifulSoup(response, 'html.parser')
+
+print(soup.prettify())
+
+#Bingung yang diambil yang mana karena hanya pesan redirect
