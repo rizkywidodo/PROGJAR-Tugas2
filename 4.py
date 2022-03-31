b67e67c37e396ca44ce6b9387241e496768e01ed
rizkywidodo <74779025+rizkywidodo@users.noreply.github.com>
2022-03-31 11:32:26 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,17 +1,12 @@
+from bs4 import BeautifulSoup
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 url = "classroom.its.ac.id"
 sock.connect((url, 80))
-sock.send(b"GET / HTTP/1.1\r\nHost:classroom.its.ac.id\r\n\r\n")
+sock.send(b"GET / HTTP/1.1\r\nHost:its.ac.id\r\n\r\n")
 response = sock.recv(4096)
 
-response = response.decode()
-print(response)
-# response = response.split("\r\n")
-# content_type = response[3].split(' ')[1]
-# charset = content_type.split('/')[1]
+soup = BeautifulSoup(response, 'html.parser')
 
-sock.close()
-
-# BELUM
+print(soup.original_encoding)
